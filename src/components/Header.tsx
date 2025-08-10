b945d748035e927a8f3d79ae34fe846bd7811573
NAIMS Enterprise (Private Limited) <naimsenterprise.office@gmail.com>
2025-08-10 18:34:14 +0000

    do 28x28 px

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -92,7 +92,7 @@ export function Header() {
           <div className="flex items-center gap-2 md:hidden">
             <Button asChild variant="ghost" size="icon">
                 <Link href="/calculate">
-                    <Calculator className="h-6 w-6" />
+                    <Calculator className="h-7 w-7" />
                     <span className="sr-only">Calculate</span>
                 </Link>
             </Button>
