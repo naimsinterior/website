851ec907b4609887062452674c754b2dbb93d188
NAIMS Enterprise (Private Limited) <naimsenterprise.office@gmail.com>
2025-08-10 18:06:47 +0000

    you can search on google NAIMS INTERIOR and than update google maps

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -34,35 +34,35 @@ const locations = [
         address: 'H-77, Silai Bara Gaon, Milak, Rampur, Uttar Pradesh (244701)',
         email: 'headoffice@naimsinterior.com',
         phone: '(+91) 987-654-3210',
-        mapSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3475.253634312898!2d79.0270923151009!3d29.42145398211239!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390a475b7a1e1b7b%3A0x8e8e8e8e8e8e8e8e!2sRampur%2C%20Uttar%20Pradesh!5e0!3m2!1sen!2sin!4v1622546777893!5m2!1sen!2sin"
+        mapSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d55604.57684847901!2d79.0026363872999!3d28.8092348510808!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390a5add696144a1%3A0x2d87a7168b958937!2sRampur%2C%20Uttar%20Pradesh!5e0!3m2!1sen!2sin!4v1719488825852!5m2!1sen!2sin"
     },
     {
         name: 'Noida',
         address: 'Sector 62, Noida, Uttar Pradesh 201309',
         email: 'noida@naimsinterior.com',
         phone: '(+91) 987-654-3211',
-        mapSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3503.220899320291!2d77.3712563150818!3d28.59318198243325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cef7d5f5f5f5f%3A0x3f3f3f3f3f3f3f3f!2sSector%2062%2C%20Noida%2C%20Uttar%20Pradesh!5e0!3m2!1sen!2sin!4v1622546888999!5m2!1sen!2sin"
+        mapSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14013.149595299837!2d77.36151909923838!3d28.608759600000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce544da318685%3A0x62836b6901844893!2sSector%2062%2C%20Noida%2C%20Uttar%20Pradesh!5e0!3m2!1sen!2sin!4v1719488924296!5m2!1sen!2sin"
     },
     {
         name: 'Gurgaon',
         address: 'DLF Cyber City, Gurgaon, Haryana 122002',
         email: 'gurgaon@naimsinterior.com',
         phone: '(+91) 987-654-3212',
-        mapSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3507.962551528628!2d77.0864349150787!3d28.45055098248983!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1f5d5f5f5f5f%3A0x1f1f1f1f1f1f1f1f!2sDLF%20Cyber%20City%2C%20Gurugram%2C%20Haryana!5e0!3m2!1sen!2sin!4v1622546999000!5m2!1sen!2sin"
+        mapSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14031.1194200159!2d77.0805335992225!3d28.4984242!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d194473c4d42d%3A0x891435fc036e5229!2sDLF%20CyberHub%2C%20Gurugram%2C%20Haryana!5e0!3m2!1sen!2sin!4v1719489000185!5m2!1sen!2sin"
     },
     {
         name: 'Chennai',
         address: 'T. Nagar, Chennai, Tamil Nadu 600017',
         email: 'chennai@naimsinterior.com',
         phone: '(+91) 987-654-3213',
-        mapSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3886.620197732049!2d80.2315189148227!3d13.06041699079717!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5266988f5f5f5f%3A0x9f9f9f9f9f9f9f9f!2sT.%20Nagar%2C%20Chennai%2C%20Tamil%20Nadu!5e0!3m2!1sen!2sin!4v1622547111222!5m2!1sen!2sin"
+        mapSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31093.003926019313!2d80.21985901309832!3d13.04586940801304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a526674a2373307%3A0x356f955f01905380!2sT.%20Nagar%2C%20Chennai%2C%20Tamil%20Nadu!5e0!3m2!1sen!2sin!4v1719489053531!5m2!1sen!2sin"
     },
     {
         name: 'OMR',
         address: 'Old Mahabalipuram Road, Chennai, Tamil Nadu 600119',
         email: 'omr@naimsinterior.com',
         phone: '(+91) 987-654-3214',
-        mapSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.995977926189!2d80.2209736148212!3d12.90799699089823!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a525d5d5f5f5f5f%3A0x7f7f7f7f7f7f7f7f!2sOld%20Mahabalipuram%20Road%2C%20Chennai%2C%20Tamil%20Nadu!5e0!3m2!1sen!2sin!4v1622547222333!5m2!1sen!2sin"
+        mapSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d124434.9080649547!2d80.12119934339486!3d12.898867372776489!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a525c3458448f47%3A0x959733477348981d!2sOld%20Mahabalipuram%20Rd%2C%20Tamil%20Nadu!5e0!3m2!1sen!2sin!4v1719489110196!5m2!1sen!2sin"
     }
 ];
 
